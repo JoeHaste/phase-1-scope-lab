11d0642b22596b8304d46cfdfd2ffa36fdb90b70
Joseph Karanja <joseph.karanja@student.moringaschool.com>
2023-06-17 15:38:27 +0300

    complete assignement

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob'
+const LeastFavoriteCustomer = 'Andre'
+
+function upperCaseCustomerName(customer){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+     bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(customer1){
+    LeastFavoriteCustomer = 'Pete'
+}
